cf69721e6ed55f768507cce590b16f9b30bfb62e
Joseph Csoti <josephcsoti@gmail.com>
2017-10-23 01:24:41 -0700

    Added tests using Firebase Shell

---
+++ functions/test/testing.js
@@ -0,0 +1,115 @@
+// Data
+const data = {
+  users: [
+    {uid: 'TEST_USER_ID_1', email: 'TEST_EMAIL_1@tracestudios.xyz'},
+    {uid: 'TEST_USER_ID_2', email: 'TEST_EMAIL_2@tracestudios.xyz'},
+    {uid: 'TEST_USER_ID_3', email: 'TEST_EMAIL_3@tracestudios.xyz'},
+    {uid: 'TEST_USER_ID_4', email: 'TEST_EMAIL_4@tracestudios.xyz'},
+    {uid: 'TEST_USER_ID_5', email: 'TEST_EMAIL_5@tracestudios.xyz'},
+    {uid: 'TEST_USER_ID_6', email: 'TEST_EMAIL_6@tracestudios.xyz'},
+    {uid: 'TEST_USER_ID_7', email: 'TEST_EMAIL_7@tracestudios.xyz'},
+    {uid: 'TEST_USER_ID_8', email: 'TEST_EMAIL_8@tracestudios.xyz'},
+    {uid: 'TEST_USER_ID_9', email: 'TEST_EMAIL_9@tracestudios.xyz'},
+    {uid: 'TEST_USER_ID_A', email: 'TEST_EMAIL_A@tracestudios.xyz'},
+  ],
+  lobbies: [
+    'LOBBY_ID_A',
+    'LOBBY_ID_B'
+  ],
+  pushkeys: [
+    'PUSH_KEY_1',
+    'PUSH_KEY_2',
+    'PUSH_KEY_3',
+    'PUSH_KEY_4',
+    'PUSH_KEY_5',
+    'PUSH_KEY_6',
+    'PUSH_KEY_7',
+    'PUSH_KEY_8',
+    'PUSH_KEY_9',
+    'PUSH_KEY_A',
+  ]
+};
+
+// Data Helpers
+var users = data.users;
+var lobbies = data.lobbies;
+var pushkeys = data.pushkeys;
+
+// Consts
+const LOBBY_COUNT = 2;
+const SPLIT_POINT = ((users.length-LOBBY_COUNT) / 2) + LOBBY_COUNT; //Default is 1/2
+
+// Create new user
+for(var i=0; i<users.length; i++){
+  Register({
+    uid: users[i].uid, 
+    email: users[i].email
+  })
+}
+
+// Create Lobbies
+for(var i=0; i<LOBBY_COUNT; i++){
+  CreateLobby({
+    userID: users[i].uid},
+    {
+      auth: {variable: {uid: users[i].uid}},
+      params: {pushkey: lobbies[i]}
+    }
+  )
+}
+
+// Join lobbies
+for(var i=LOBBY_COUNT; i<SPLIT_POINT; i++){
+  JoinLobby({
+    lobbyID: lobbies[0],
+    userID: users[i].uid},
+    {
+      params: {pushkey: pushkeys[i]}
+    }
+  )
+}
+for(var i=split_point; i<users.length; i++){
+  JoinLobby({
+    lobbyID: lobbies[1],
+    userID: users[i].uid},
+    {
+      params: {pushkey: pushkeys[i]}
+    }
+  ) 
+}
+
+// Update Player Count
+
+// Empty player sets
+var players_0 = {}
+var players_a = {};
+var players_b = {};
+
+// Add "Leaders" seperately 
+players_a[users[0].uid] = true;
+players_b[users[1].uid] = true;
+
+// Lobby A
+for(var i=LOBBY_COUNT; i<SPLIT_POINT; i++){
+  players_a[users[i].uid] = true;
+}
+// Lobby B
+for(var i=SPLIT_POINT; i<users.length; i++){
+  players_b[users[i].uid] = true;
+}
+// Lobby A
+UpdateLobbyPlayerCount(
+  {
+    before: players_0,
+    after: players_a
+  },
+  {params: {pushkey: lobbies[0]}}
+)
+// Lobby B
+UpdateLobbyPlayerCount(
+  {
+    before: players_0,
+    after: players_b
+  },
+  {params: {pushkey: lobbies[1]}}
+)
